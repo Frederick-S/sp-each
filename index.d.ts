7e81f88fbb194d1cd01820ff216e9dfb13800a99
Frederick-S <mao_xiaodan@hotmail.com>
2016-06-23 01:10:21 +0000

    Add tslint

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 export interface IIteratee<T> {
     (element: T, index: number, collection: IEnumerable<T>): void;
 }
+
 export declare function each<T>(collection: IEnumerable<T>, iteratee: IIteratee<T>, context?: any): void;
